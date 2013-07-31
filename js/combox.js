92037fcccc34ca545cb61d2d3cff48be2955e240
Sergii Bomko <aquila.ua@gmail.com>
2013-07-31 07:55:52 +0300

    refactoring

--- js/combox.js
+++ js/combox.js
@@ -8,7 +8,7 @@
 (function( $, undefined ) { 'use strict';
 	$.widget('custom.combox', {
 		version: '0.1',
-		defaultElement: "<div>",
+		defaultElement: '<div>',
 		options: {
 			minHeight: 20
 		},
@@ -17,11 +17,12 @@
 			var options = this.options;
 						
 			this._draw();
-			this._on( this._events );
+			this._on(this._events);
+			this._refresh();
 		},
 		
 		_events: {
-			'click .ui-combox-select': function( event ) {
+			'click .ui-combox-select': function(event) {
 				var selectDiv = $(event.currentTarget);
 				var position = selectDiv.position();
 				selectDiv.find('ul')
@@ -31,7 +32,7 @@
 					})
 					.toggle();
 			},
-			'click .ui-combox-select li': function( event ) {
+			'click .ui-combox-select li': function(event) {
 				var item = $(event.currentTarget);
 				var selectDiv = item.closest('.ui-combox-select');
 				var option = $('#' + selectDiv.attr('src-fld') + ' option[value=' + item.val() + ']')
@@ -43,49 +44,55 @@
 		
 		_draw: function() {
 			var obj = this;
-			var wrapper = $('<div></div>')
+			var combox = this.combox = this.element
 				.addClass('ui-combox');
-			this.element.wrapInner(wrapper);
-			this.comboxElement = this.element.find('.ui-combox');
 			
-			this.comboxElement
-				.find('input[type=text], select[id]')
+			//add fields
+			combox.find('input[type=text], select[id]')
 				.each(function () {
 					var field = $(this);
 					if(field.is("input"))
 					{
-						var inputWrapper = $('<div></div>')
-							.addClass('ui-combox-input');
-						field.wrap(inputWrapper);
+						field.wrap($('<div></div>')
+							.addClass('ui-combox-input'));
 					} else {
-						var height = Math.max(obj.options.minHeight, obj.comboxElement.height());
-						var selectWrapper = $('<div></div>')
-							.attr({'src-fld': field.attr('id')})
-							.addClass('ui-combox-select')
-							.css({
-								'width': field.width(),
-								'height': height
-							})
-							.append('<span></span>');
-						$('<div></div>')
-							.addClass('ui-combox-pointer')
-							.css({
-								'height': height - 4
-							})
-							.appendTo(selectWrapper);
-						var optionsWrapper = $('<ul></ul>')
-							.appendTo(selectWrapper);
-						
-						field.hide()
-							.after(selectWrapper)
-							.children()
-							.each(function() {
-								var option = $(this);
-								$('<li></li>').val(option.val())
-											  .text(option.text())
-											  .appendTo(optionsWrapper);
-							});
+						obj._buildSelectHtml(field);
 					}
+				});
+		},
+		
+		_refresh: function() {
+			this.combox
+				.find('.ui-combox-select li:first').click()
+				.parent().toggle();
+		},
+		
+		_buildSelectHtml: function(field) {
+			var height = Math.max(this.options.minHeight, this.combox.height());
+			var selectWrapper = $('<div></div>')
+				.attr({'src-fld': field.attr('id')})
+				.addClass('ui-combox-select')
+				.css({
+					'width': field.width(),
+					'height': height
+				})
+				.append('<span></span>');
+			$('<div></div>')
+				.addClass('ui-combox-pointer')
+				.css('height', height - 4)
+				.appendTo(selectWrapper);
+			var optionsWrapper = $('<ul></ul>')
+				.appendTo(selectWrapper);
+			
+			field
+				.hide()
+				.before(selectWrapper)
+				.children()
+				.each(function() {
+					var option = $(this);
+					$('<li></li>').val(option.val())
+								  .text(option.text())
+								  .appendTo(optionsWrapper);
 				});
 		}
 	});
