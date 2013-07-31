beaaa3a4fedcc48bf460847b1e9f528cd5eb1fcd
Sergii Bomko <aquila.ua@gmail.com>
2013-07-31 22:16:55 +0300

    update script, added state for select

--- js/combox.js
+++ js/combox.js
@@ -1,99 +1,152 @@
 /* ========================================================================
- * ComboxJs: combox.js v0.1 - 2013-31-07
+ * ComboxJs: combox.js v0.1 - 2013-07-31
  * https://github.com/aquiladev/comboxjs.git
  * ========================================================================
  * Copyright 2013 Sergii Bomko.
+ * ComboxJs may be freely distributed under the MIT license.
  * ======================================================================== */
 
-(function( $, undefined ) { 'use strict';
+(function ($) {
+	'use strict';
 	$.widget('custom.combox', {
 		version: '0.1',
 		defaultElement: '<div>',
 		options: {
-			minHeight: 20
+			minHeight: 20,
+			maxHeight: 32,
+			minSelectsWidth: 80,
+			minInputsWidth: 80
 		},
-		
-		_create: function() {
-			var options = this.options;
-						
+
+		_create: function () {
 			this._draw();
 			this._on(this._events);
 			this._refresh();
 		},
-		
+
 		_events: {
-			'click .ui-combox-select': function(event) {
+			'click .ui-combox-select': function (event) {
 				var selectDiv = $(event.currentTarget);
-				var position = selectDiv.position();
+				window.clearTimeout(this.timer);
 				selectDiv.find('ul')
 					.css({
 						'top': selectDiv.height(),
+						'left': -1,
 						'width': selectDiv.width()
 					})
 					.toggle();
 			},
-			'click .ui-combox-select li': function(event) {
+			'click .ui-combox-select li': function (event) {
 				var item = $(event.currentTarget);
+				this._refreshSelect(item);
 				var selectDiv = item.closest('.ui-combox-select');
-				var option = $('#' + selectDiv.attr('src-fld') + ' option[value=' + item.val() + ']')
-					.attr('selected', 'selected')
-					.prop('selected', true);
-				selectDiv.find('span').text(item.text());
+				$('#' + selectDiv.attr('src-fld')).trigger('change');
+			},
+			'mouseleave .ui-combox-select ul': function (event) {
+				var list = $(event.currentTarget);
+				this.timer = this._delay(function () {
+					list.hide();
+				}, 200);
 			}
 		},
-		
-		_draw: function() {
+
+		_draw: function () {
 			var obj = this;
 			var combox = this.combox = this.element
 				.addClass('ui-combox');
-			
+
 			//add fields
 			combox.find('input[type=text], select[id]')
 				.each(function () {
 					var field = $(this);
-					if(field.is("input"))
-					{
-						field.wrap($('<div></div>')
-							.addClass('ui-combox-input'));
+					if (field.is("input")) {
+						obj._buildInputHtml(field);
 					} else {
 						obj._buildSelectHtml(field);
 					}
 				});
 		},
-		
-		_refresh: function() {
-			this.combox
-				.find('.ui-combox-select li:first').click()
-				.parent().toggle();
+
+		_refresh: function () {
+			var item = this.combox.find(".ui-combox-select li[selected]");
+			if (item.length == 0) {
+				item = this.combox.find('.ui-combox-select li:first');
+			}
+			this._refreshSelect(item);
+			item.removeAttr("selected")
+				.parent().hide();
 		},
 		
-		_buildSelectHtml: function(field) {
+		_refreshSelect: function(item) {
+			var selectDiv = item.closest('.ui-combox-select');
+			var select = $('#' + selectDiv.attr('src-fld'));
+			var option = select.find('option[value=' + item.attr('value') + ']');
+			if (option.length == 0) {
+				option = select.find('option:first');
+			}
+			select
+				.children("option:selected")
+				.removeAttr("selected")
+				.prop('selected', false);
+			option
+				.attr('selected', 'selected')
+				.prop('selected', true);
+			selectDiv.find('label').text(item.text());
+		},
+
+		_buildInputHtml: function (field) {
+			var width = Math.max(this.options.minInputsWidth, field.width());
+			var inputWrapper = $('<div></div>')
+				.addClass('ui-combox-input')
+				.css({
+					'width': width,
+					'max-height': this.options.maxHeight,
+					'min-width': this.options.minInputsWidth
+				});
+			field.wrap(inputWrapper)
+				.width('100%');
+		},
+		
+		_buildSelectHtml: function (field) {
 			var height = Math.max(this.options.minHeight, this.combox.height());
+			var width = Math.max(this.options.minSelectsWidth, field.width());
 			var selectWrapper = $('<div></div>')
-				.attr({'src-fld': field.attr('id')})
+				.attr({ 'src-fld': field.attr('id') })
 				.addClass('ui-combox-select')
 				.css({
-					'width': field.width(),
-					'height': height
+					'width': width,
+					'min-width': this.options.minSelectsWidth,
+					'height': height,
+					'max-height': this.options.maxHeight,
+					'line-height': height + 'px'
 				})
-				.append('<span></span>');
+				.append('<label></label>');
+			
+			//adding pointer
 			$('<div></div>')
 				.addClass('ui-combox-pointer')
 				.css('height', height - 4)
 				.appendTo(selectWrapper);
+
+			//adding options
 			var optionsWrapper = $('<ul></ul>')
+				.css({
+					'width': width,
+					'min-width': this.options.minSelectsWidth
+				})
 				.appendTo(selectWrapper);
-			
+
 			field
 				.hide()
 				.before(selectWrapper)
 				.children()
-				.each(function() {
+				.each(function () {
 					var option = $(this);
-					$('<li></li>').val(option.val())
-								  .text(option.text())
+					$('<li></li>').text(option.text())
+								  .attr('value', option.val())
+								  .attr('selected', option.is(':selected'))
 								  .appendTo(optionsWrapper);
 				});
 		}
 	});
-})( jQuery );
+})(jQuery);
